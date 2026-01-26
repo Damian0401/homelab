f8c4852227047889f5e50532c7a04a171a38823a
Damian Gnieciak <szkola_abc@o2.pl>
2026-01-26 21:39:32 +0100

    Added telegraf for system metrics

--- influxdb-init.sh
+++ influxdb-init.sh
@@ -4,7 +4,7 @@ set -e
 echo "Initializing InfluxDB database..."
 
 influx bucket create \
-    --name system_metrics \
+    --name "$DOCKER_INFLUXDB_SYSTEM_METRICS_BUCKET" \
     --retention 30d \
     --org "$DOCKER_INFLUXDB_INIT_ORG" \
     --token "$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"
