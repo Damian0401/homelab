f3f1f7b8b1a09be016c44b730599e8aaf7fef8ac
Damian Gnieciak <szkola_abc@o2.pl>
2026-03-21 20:17:35 +0100

    Added sensors bucket

--- influxdb/influxdb-init.sh
+++ influxdb/influxdb-init.sh
@@ -9,4 +9,10 @@ influx bucket create \
     --org "$DOCKER_INFLUXDB_INIT_ORG" \
     --token "$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"
 
+influx bucket create \
+    --name "$DOCKER_INFLUXDB_SENSORS_BUCKET" \
+    --retention 90d \
+    --org "$DOCKER_INFLUXDB_INIT_ORG" \
+    --token "$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"
+
 echo "InfluxDB initialization complete."
